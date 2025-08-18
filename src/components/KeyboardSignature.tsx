46163c5d7a76a852579ba0208b452cd57a278991
cnrad <conradwcrawford@gmail.com>
2025-08-18 00:05:23 -0400

    feat:  babyyy

--- src/components/KeyboardSignature.tsx
+++ src/components/KeyboardSignature.tsx
@@ -146,6 +146,19 @@ export const KeyboardSignature = () => {
     <div
       className={`flex flex-col sm:items-center sm:justify-center max-sm:mx-auto max-sm:w-[28rem] sm:w-fit`}
     >
+      <a
+        className="absolute cursor-pointer left-1/2 bottom-8 -translate-x-1/2 opacity-50 hover:opacity-100 text-sm flex flex-col items-center transition-all duration-150 ease-out text-center"
+        href="https://axiom.trade/meme/3URGpspzJUT7d3LN1kfbjm6koy5fNQTgQqDY8Zh1rZ8H"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        <p className="font-bold text-white text-base">$SIGN</p>
+        <p className=" text-neutral-300 max-sm:break-all">
+          <span className="text-neutral-400 font-bold">CA:</span>{" "}
+          GjbLHUmyUo6JFczvaTbsj9p1LjsXmvR8Vk9gRPNLBAGS
+        </p>
+      </a>
+
       <input
         autoFocus
         ref={inputRef}
